881bc302ac062a7031884bf1bccab269879d5170
Tulio Goncalves <tuliogoncalves@gmail.com>
2022-06-13 09:37:22 -0400

    Docker Starter

--- docker/starter.sh
+++ docker/starter.sh
@@ -1,66 +1,35 @@
 #!/usr/bin/env bash
 
-UNAMEOUT="$(uname -s)"
+docker info > /dev/null 2>&1
 
-# Verify operating system is supported...
-case "${UNAMEOUT}" in
-    Linux*)             MACHINE=linux;;
-    Darwin*)            MACHINE=mac;;
-    *)                  MACHINE="UNKNOWN"
-esac
-
-if [ "$MACHINE" == "UNKNOWN" ]; then
-    echo "Unsupported operating system [$(uname -s)]. Laravel Sail supports macOS, Linux, and Windows (WSL2)." >&2
+# Ensure that Docker is running...
+if [ $? -ne 0 ]; then
+    echo "Docker is not running."
 
     exit 1
 fi
 
-# Determine if stdout is a terminal...
-if test -t 1; then
-    # Determine if colors are supported...
-    ncolors=$(tput colors)
-
-    if test -n "$ncolors" && test "$ncolors" -ge 8; then
-        BOLD="$(tput bold)"
-        YELLOW="$(tput setaf 3)"
-        GREEN="$(tput setaf 2)"
-        NC="$(tput sgr0)"
-    fi
-fi
-
-# Source the ".env" file so Laravel's environment variables are available...
-if [ -f ./.env ]; then
-    source ./.env
-fi
-
-# Define environment variables...
-export APP_PORT=${APP_PORT:-80}
-export APP_SERVICE=${APP_SERVICE:-"laravel.test"}
-export DB_PORT=${DB_PORT:-3306}
-export WWWUSER=${WWWUSER:-$UID}
-export WWWGROUP=${WWWGROUP:-$(id -g)}
-
-export SAIL_FILES=${SAIL_FILES:-""}
-export SAIL_SHARE_DASHBOARD=${SAIL_SHARE_DASHBOARD:-4040}
-export SAIL_SHARE_SERVER_HOST=${SAIL_SHARE_SERVER_HOST:-"laravel-sail.site"}
-export SAIL_SHARE_SERVER_PORT=${SAIL_SHARE_SERVER_PORT:-8080}
-export SAIL_SHARE_SUBDOMAIN=${SAIL_SHARE_SUBDOMAIN:-""}
-
-
-if [ -z "$SAIL_SKIP_CHECKS" ]; then
-    # Ensure that Docker is running...
-    if ! docker info > /dev/null 2>&1; then
-        echo "${BOLD}Docker is not running.${NC}" >&2
-
-        exit 1
-    fi
-fi
-
-docker-compose build 
-
 docker run --rm \
-    -u "$(id -u):$(id -g)" \
-    -v $(pwd):/var/www/html \
-    -w /var/www/html \
-    app \
-    composer install --ignore-platform-reqs
+    -v "$(pwd)":/opt \
+    -w /opt \
+    laravelsail/php74-composer:latest \
+    bash -c "composer install --ignore-platform-reqs"
+
+CYAN='\033[0;36m'
+LIGHT_CYAN='\033[1;36m'
+WHITE='\033[1;37m'
+NC='\033[0m'
+
+echo ""
+
+if sudo -n true 2>/dev/null; then
+    sudo chown -R $USER: .
+    echo -e "${WHITE}Get started with:${NC} ./vendor/bin/sail up"
+else
+    echo -e "${WHITE}Please provide your password so we can make some final adjustments to your application's permissions.${NC}"
+    echo ""
+    sudo chown -R $USER: .
+    echo ""
+    echo -e "${WHITE}Thank you! We hope you build something incredible. Dive in with:${NC} ./vendor/bin/sail up"
+fi
+
