47dbc843cfd5af9a0dd1e813209902a81902e9f3
Tulio Gonçalves <tuliogoncalves@gmail.com>
2023-06-19 22:45:09 -0400

    create crud validator

--- src/framework/Repository/Crud/Validation.php
+++ src/framework/Repository/Crud/Validation.php
@@ -8,91 +8,7 @@ use Illuminate\Contracts\Validation\Validator as IValidator;
 
 abstract class Validation
 {
-    /**
-     * The validator instance.
-     *
-     * @var IValidator
-     */
-    protected $validator;
-
-    // function __construct()
-    // {
-    // }
-
     abstract function rules();
-
-    /**
-     * Set the Validator instance.
-     *
-     * @param  IValidator  $validator
-     * @return $this
-     */
-    public function setValidator(IValidator $validator)
-    {
-        $this->validator = $validator;
-
-        return $this;
-    }
-
-    /**
-     * Determine if the request passes the authorization check.
-     *
-     * @return bool
-     */
-    protected function passesAuthorization()
-    {
-        if (method_exists($this, 'authorize')) {
-            return $this->authorize();
-        }
-
-        return true;
-    }
-
-    /**
-     * Get the validator instance for the request.
-     *
-     * @return IValidator
-     */
-    protected function getValidatorInstance()
-    {
-        if ($this->validator) {
-            return $this->validator;
-        }
-
-        if (method_exists($this, 'validator')) {
-            $validator = $this->validator();
-        } else {
-            $validator = $this->createDefaultValidator();
-        }
-
-        if (method_exists($this, 'withValidator')) {
-            $this->withValidator($validator);
-        }
-
-        if (method_exists($this, 'after')) {
-            $validator->after($this->container->call(
-                $this->after(...),
-                ['validator' => $validator]
-            ));
-        }
-
-        $this->setValidator($validator);
-
-        return $this->validator;
-    }
-
-    /**
-     * Create the default validator instance.
-     *
-     * @param  \Illuminate\Contracts\Validation\Factory $factory
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
-    public function getRules()
-    {
-        $rules = method_exists($this, 'rules') ? $this->rules() : [];
-
-        return $rules;
-    }
 
     /**
      * Prepare the data for validation.
@@ -132,5 +48,61 @@ abstract class Validation
     public function attributes()
     {
         return [];
+    }
+
+    /**
+     * Summary of getRules
+     * @return mixed
+     */
+    public function getRules()
+    {
+        $rules = method_exists($this, 'rules') ? $this->rules() : [];
+
+        return $rules;
+    }
+
+    /**
+     * Determine if the request passes the authorization check.
+     *
+     * @return bool
+     */
+    function passesAuthorization()
+    {
+        if (method_exists($this, 'authorize')) {
+            return $this->authorize();
+        }
+
+        return true;
+    }
+
+    /**
+     * Get the validator instance for the request.
+     *
+     * @return IValidator
+     */
+    protected function getValidatorInstance()
+    {
+        if ($this->validator) {
+            return $this->validator;
+        }
+
+        if (method_exists($this, 'validator')) {
+            $validator = $this->validator();
+        } else {
+            $validator = $this->createDefaultValidator();
+        }
+
+        if (method_exists($this, 'withValidator')) {
+            $this->withValidator($validator);
+        }
+
+        if (method_exists($this, 'after')) {
+            $validator->after($this->container->call(
+                $this->after(...),
+                ['validator' => $validator]
+            ));
+        }
+
+        return $this->validator;
     }
 }
